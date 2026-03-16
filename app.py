0d2d62539fc75b083e91262beb4dadee332c8e78
Neethika Reddy <chittyreddyneethika@gmail.com>
2026-03-16 11:20:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask
-import os
+from flask import Flask, jsonify
+import cv2
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Traffic Signal Server Running"
+def calculate_density(image):
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,50,150)
+
+    vehicle_pixels = np.sum(edges > 0)
+
+    density = vehicle_pixels / image.size
+
+    return density
+
+
+def signal_time(density):
+
+    if density < 0.02:
+        return 10
+    elif density < 0.05:
+        return 20
+    elif density < 0.08:
+        return 30
+    else:
+        return 40
+
+
+@app.route("/")
+def analyze():
+
+    results = {}
+
+    for i in range(1,5):
+
+        image = cv2.imread(f"images/lane{i}.jpg")
+
+        density = calculate_density(image)
+
+        time = signal_time(density)
+
+        results[f"lane{i}"] = {
+            "density": float(density),
+            "green_signal_time": time
+        }
+
+    return jsonify(results)
+
+
+app.run(host="0.0.0.0", port=10000)
